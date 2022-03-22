2bdbedbbbe3ff75b1bd468ae91f123d80b84efae
hugosenari <hugosenari@gmail.com>
2022-03-22 01:18:02 -0300

    chore: re-index ../nimpkgs/t/timezones

--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-timezones-v0_2_0.flake = false;
+  inputs.src-timezones-v0_2_0.ref   = "refs/tags/v0.2.0";
   inputs.src-timezones-v0_2_0.owner = "GULPF";
-  inputs.src-timezones-v0_2_0.ref   = "v0_2_0";
   inputs.src-timezones-v0_2_0.repo  = "timezones";
   inputs.src-timezones-v0_2_0.type  = "github";
   
--- v0_2_1/flake.nix
+++ v0_2_1/flake.nix
@@ -8,8 +8,8 @@
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
   inputs.src-timezones-v0_2_1.flake = false;
+  inputs.src-timezones-v0_2_1.ref   = "refs/tags/v0.2.1";
   inputs.src-timezones-v0_2_1.owner = "GULPF";
-  inputs.src-timezones-v0_2_1.ref   = "v0_2_1";
   inputs.src-timezones-v0_2_1.repo  = "timezones";
   inputs.src-timezones-v0_2_1.type  = "github";
   
